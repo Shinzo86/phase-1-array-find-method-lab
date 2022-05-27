97ddade268e93433f8b5889dbcd5de97ea422bcd
Adrian Alberto <adrianalberto1986@gmail.com>
2022-05-27 17:56:48 -0500

    commit tree 0874df4876e62c2cbe2dddc3b0ab3fd97081cc0c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// function isAWin(result){
+//     return (result.result === 'W')
+// }
+
+// function superbowlWin(record){
+//     const found = record.find(isAWin)
+//     if(found){
+//         return found.year
+//     }else{
+//         return undefined
+//     }
+// }
+
+
+function superbowlWin(records){
+    const win = records.find((element) => (element.result === 'W'))
+    return win ? win.year : undefined
+}
